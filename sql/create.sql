f31818371a9d199201101e8e4af362056351744f
frussian <zoomod57@gmail.com>
2022-04-06 22:26:31 +0300

    create update

--- sql/create.sql
+++ sql/create.sql
@@ -1,27 +1,53 @@
--- Database: musicdb
--- DROP DATABASE IF EXISTS musicdb;
-
--- CREATE DATABASE musicdb
---     WITH
---     OWNER = anton
---     ENCODING = 'UTF8'
---     LC_COLLATE = 'English_United States.1252'
---     LC_CTYPE = 'English_United States.1252'
---     TABLESPACE = pg_default
---     CONNECTION LIMIT = -1;
-
--- GRANT ALL ON DATABASE musicdb TO anton;
---
--- GRANT ALL ON DATABASE musicdb TO postgres;
---
--- GRANT TEMPORARY, CONNECT ON DATABASE musicdb TO PUBLIC;
---
--- SELECT version();
-drop table if exists Musician;
-
+drop table if exists Musician cascade;
 create table Musician (
-	MusicianID serial primary key,
-	DateOfBirth date not null,
-	Biography text,
-	MusicianName varchar(32) unique not null
+	musicianID int generated always as identity primary key,
+	musicianName varchar(32) unique not null,
+	biography text,
+	dateOfBirth date,
+	img oid
 );
+
+drop type instr_enum cascade;
+create type instr_enum as enum ('guitar', 'bass', 'vocal', 'drums');
+
+drop table if exists Instrument cascade;
+create table Instrument (
+	"type" instr_enum unique not null
+);
+
+drop table if exists MusInstrInt;
+create table MusInstrInt (
+	musicianID int references Musician(musicianId),
+	"type" instr_enum references Instrument("type"),
+	primary key(musicianID, "type")
+);
+
+
+drop type genre_enum cascade;
+create type genre_enum as enum('rock', 'alternative', 'indie', 'blues', 'metal');
+
+drop table if exists Band cascade;
+create table Band (
+	bandID int generated always as identity primary key,
+	bandName varchar(64) unique not null,
+	genre genre_enum,
+	foundingDate date,
+	terminationDate date,
+	img oid
+);
+
+drop table if exists Membership cascade;
+create table Membership (
+	membID int generated always as identity primary key,
+	musName varchar(32) references Musician(musicianName),
+	bandName varchar(32) references Band(bandName),
+	enterDate date not null,
+	quitDate date,
+	unique (musName, bandName, enterDate)
+);
+
+
+
+
+
+
